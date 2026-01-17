49d55ae98f62f52d1661ce6f1110337b39c775aa
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2026-01-17 21:20:14 +0700

    1388-GREATEST-SUM-DIVISIBLE-BY-THREE Stats: Time: 46 ms (12.44%), Space: 37.1 MB (18.35%) - LeetHub

--- 1388-greatest-sum-divisible-by-three/1388-greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/1388-greatest-sum-divisible-by-three.cpp
@@ -1,68 +1,20 @@
 class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
-        priority_queue<int> remainder_one;
-        priority_queue<int> remainder_two;
+        vector<int> dp(3, 0);
 
-        int sum = 0;
-        for (auto num : nums) {
-            sum += num;
+        for (auto &num: nums){
+            vector<int> next_dp = dp;
 
-            if (num % 3 == 1) {
-                remainder_one.push(num);
-                if (remainder_one.size() > 2) {
-                    remainder_one.pop();
-                }
-            } else if (num % 3 == 2) {
-                remainder_two.push(num);
-                if (remainder_two.size() > 2) {
-                    remainder_two.pop();
-                }
+            for (int s: dp){
+                int next_sum = s + num;
+                int remainder = next_sum % 3;
+                next_dp[remainder] = max(next_sum, next_dp[remainder]);
             }
+
+            dp = next_dp;
         }
 
-        if (sum % 3 == 0) {
-            return sum;
-        } else if (sum % 3 == 1) {
-            int min1;
-            while (!remainder_one.empty()) {
-                min1 = remainder_one.top();
-                remainder_one.pop();
-            }
-
-            if (remainder_two.size() >= 2) {
-                int num1 = remainder_two.top();
-                remainder_two.pop();
-                int num2 = remainder_two.top();
-
-                if (num1 + num2 < min1) {
-                    return sum - num1 - num2;
-                } else {
-                    return sum - min1;
-                }
-            } else {
-                return sum - min1;
-            }
-        } else {
-            int min2;
-            while (!remainder_two.empty()) {
-                min2 = remainder_two.top();
-                remainder_two.pop();
-            }
-            
-            if (remainder_one.size() >= 2) {
-                int num1 = remainder_one.top();
-                remainder_one.pop();
-                int num2 = remainder_one.top();
-
-                if (num1 + num2 < min2) {
-                    return sum - num1 - num2;
-                } else {
-                    return sum - min2;
-                }
-            } else {
-                return sum - min2;
-            }
-        }
+        return dp[0];
     }
 };
