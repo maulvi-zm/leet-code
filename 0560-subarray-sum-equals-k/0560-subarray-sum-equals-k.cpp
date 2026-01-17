1a5ee30b09d6ad6848687104a5724322cfa16d80
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2026-01-17 19:48:46 +0700

    0560-SUBARRAY-SUM-EQUALS-K Stats: Time: 35 ms (87.43%), Space: 45.4 MB (69.87%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
+    // We can't use sliding window because the array it's not monotonic
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        unordered_map<int, int> mp;
+        unordered_map<int, int> prefix_sum;
 
         int curr_sum = 0, ans = 0;
-        for (auto& num : nums) {
+
+        // Base case: prefix sum of 0 appears once
+        prefix_sum[0] = 1;
+        
+        for (auto &num : nums){
             curr_sum += num;
 
-            if (curr_sum == k) {
-                ans++;
+            // Note that: curr_sum - (curr_sum - k) = k
+            // It's the basic of prefix_sum where sum(i, j) = prefix[i] - prefix[j-1]
+            // So we just need to see if there's any subarray that has (curr_sum - k) value
+            if (prefix_sum.find(curr_sum - k) != prefix_sum.end()){
+                ans += prefix_sum[curr_sum - k];
             }
 
-            if (mp.count(curr_sum - k)) {
-                ans += mp[curr_sum - k];
-            }
-
-            if (mp.count(curr_sum) == 0) {
-                mp[curr_sum] = 1;
-            } else {
-                mp[curr_sum] += 1;
-            }
+            prefix_sum[curr_sum]++;
         }
 
         return ans;
