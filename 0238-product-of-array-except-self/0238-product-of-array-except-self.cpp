8ac289a584f5154dc3b7c99ea00cee33877e51b1
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2026-01-26 17:11:40 +0700

    0238-PRODUCT-OF-ARRAY-EXCEPT-SELF Stats: Time: 0 ms (100%), Space: 42.4 MB (8.49%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,25 +4,17 @@ public:
         int n = nums.size();
         vector<int> prefix(n, 1), suffix(n, 1);
 
-        prefix[0] = nums[0];
         for (int i = 1; i < n; i++){
-            prefix[i] = nums[i] * prefix[i-1];
+            prefix[i] = nums[i-1] * prefix[i-1];
         }
 
-        suffix[n-1] = nums[n-1];
         for (int i = n - 2; i >= 0; i--){
-            suffix[i] = nums[i] * suffix[i+1];
+            suffix[i] = nums[i+1] * suffix[i+1];
         }
-
-        vector<int> ans;
+        
+        vector<int> ans(n, 0);
         for (int i = 0; i < n; i++){
-            if (i == 0){
-                ans.push_back(suffix[i+1]);
-            } else if (i == n - 1){
-                ans.push_back(prefix[i-1]);
-            } else {
-                ans.push_back(prefix[i-1]*suffix[i+1]);
-            }
+            ans[i] = prefix[i] * suffix[i];
         }
 
         return ans;
