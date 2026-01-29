f87ab8f3849043a3e55559ebd4ef929ed16d4212
Maulvi Ziadinda Maulana <code@maulvizm.in>
2026-01-29 11:04:14 +0700

    0125-VALID-PALINDROME Stats: Time: 0 ms (100%), Space: 9.9 MB (77.2%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         int l = 0, r = s.size() - 1;
-        while (l <= r){
-            while (r > 0 && !isalnum(s[r])){
+
+        while (l < r){
+            if (!isalnum(s[l])){
+                l++;
+            } else if (!isalnum(s[r])){
+                r--;
+            } else {
+                if (tolower(s[r]) != tolower(s[l])){
+                    return false;
+                }
+
+                l++;
                 r--;
             }
-
-            while (l < s.size() - 1 && !isalnum(s[l])){
-                l++;
-            }
-
-            if (l > r){
-                break;
-            }
-
-            if (tolower(s[r]) != tolower(s[l])){
-                return false;
-            }
-
-            l++;
-            r--;
         }
 
         return true;
